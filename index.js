aaf9a27113fba1d94b46dbe43f1647efa8301207
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-21 09:32:01 -0400

    initail

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.send("some text");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+	console.log("Listening on port %s", PORT);
+});
